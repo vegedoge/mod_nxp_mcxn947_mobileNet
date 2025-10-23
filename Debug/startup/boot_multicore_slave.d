1bb59e39e181520f8593dd10a6fdd536d84ec384
BlakeWu <yx.wu.work@gmail.com>
2025-10-23 22:06:46 +0200

    Add mobileNet data.h / Remove tracking Debug

--- Debug/startup/boot_multicore_slave.d
+++
@@ -1,4 +0,0 @@
-startup/boot_multicore_slave.o startup/boot_multicore_slave.d: \
- ../startup/boot_multicore_slave.c \
- D:\Productivity\NXP\config\frdmmcxn947_tflm_cifar10_cm33_core0\source\mcux_config.h
-D:\Productivity\NXP\config\frdmmcxn947_tflm_cifar10_cm33_core0\source\mcux_config.h:
--- Debug/startup/startup_mcxn947_cm33_core0.d
+++
@@ -1,4 +0,0 @@
-startup/startup_mcxn947_cm33_core0.o startup/startup_mcxn947_cm33_core0.d: \
- ../startup/startup_mcxn947_cm33_core0.cpp \
- D:\Productivity\NXP\config\frdmmcxn947_tflm_cifar10_cm33_core0\source\mcux_config.h
-D:\Productivity\NXP\config\frdmmcxn947_tflm_cifar10_cm33_core0\source\mcux_config.h:
